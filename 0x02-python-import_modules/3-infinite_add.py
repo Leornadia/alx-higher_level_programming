a660b8758ac1e16e977e44b55cde3cc8f9c593e2
Leornadia <leornadiajose@gmail.com>
2024-01-03 23:29:15 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+def infinite_add():
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
+    print(total)
+
+if __name__ == "__main__":
+    infinite_add()
