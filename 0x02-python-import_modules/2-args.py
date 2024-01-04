ff3468c9e8c67659c062ebbd666c0d10a1faa151
Leornadia <leornadiajose@gmail.com>
2024-01-04 06:19:21 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
-import sys
+#!/usr/bin/python3
+import sys 
 def print_args():
-    argc = len(sys.argv) - 1
-    if argc == 0:
-        print("0 arguments.", end="")
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} argument{}:".format(argc, "s"[argc==1:]), end=" ")
-    print()
-    if argc > 0:
+        print("{} arguments:".format(num_args))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
+
 if __name__ == "__main__":
     print_args()
