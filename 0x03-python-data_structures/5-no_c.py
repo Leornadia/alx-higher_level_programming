912c3f1fcc70d650add62dc466bf0017c815ce63
Leornadia <leornadiajose@gmail.com>
2024-01-09 13:00:41 -0800

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+       return "".join(ch for ch in my_string if ch not in 'cC')
