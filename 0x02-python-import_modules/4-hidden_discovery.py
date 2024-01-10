bbe3ac6988d1099352f2e9ce00c5f1a26ba4a90d
Leornadia <leornadiajose@gmail.com>
2024-01-10 12:14:33 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,7 @@
-#!/usr/bin/env python3
-import importlib
-import sys
-
-if sys.version_info < (3, 8):
-    print("This script requires Python 3.8+")
-    exit(1)
-
-hidden_module = importlib.import_module("hidden_4")
-names = dir(hidden_module)
-
-for name in sorted(names):
-    if not name.startswith("__"):
-        print(name)
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:5}".format(allf[i]))
