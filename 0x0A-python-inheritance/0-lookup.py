609b1d2d8cc03a67a70dbeebd30d33f88078bfb4
Leornadia <leornadiajose@gmail.com>
2024-02-05 02:50:44 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function to return available attributes and 
+   methods of an object
+"""
+
+def lookup(obj):
+    """Return available attributes and methods
+    """
+    return dir(obj)
