789bc6108eca3d5d8b77bdd690f1ae75ea73c4ae
Leornadia <leornadiajose@gmail.com>
2024-02-06 10:57:22 -0800

    2-append_write.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
+"""Defines a text file-reading function."""
 
-"""
-Module Description:
-This module contains a function to read a text file and print its contents to stdout.
-
-Function(s):
-- read_file(filename=""): Reads the content of the specified text file and prints it to stdout.
-
-Example Usage:
->>> read_file("example.txt")
-This is the content of example.txt.
-"""
 
 def read_file(filename=""):
-    """
-    Function Description:
-    Reads the content of the specified text file and prints it to stdout.
-
-    Parameters:
-    - filename (str): The name of the text file to be read. Defaults to an empty string.
-
-    Returns:
-    None
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
