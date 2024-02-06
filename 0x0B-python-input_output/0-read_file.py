7b9f8a992d0664fbc6cc14b1b4759a9cea48138d
Leornadia <leornadiajose@gmail.com>
2024-02-06 09:48:25 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-"""Function to read UTF8 text file and print to stdout"""
-
 def read_file(filename=""):
-    """Read filename and print to stdout"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """
+    Function that reads a UTF-8 text file and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to be read. Defaults to "".
+
+    Returns:
+        None
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
+
