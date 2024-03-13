be4be8e1243fbd6c49fdb3bc6b7f472f5099f903
Leornadia <leornadiajose@gmail.com>
2024-03-13 07:53:49 -0700

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,7 @@
--- List all shows and their associated genres
-SELECT tv_shows.title, tv_genres.name
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- lists all rows of a table linked to another table
+SELECT title, name
 FROM tv_shows
-LEFT JOIN tv_genres_shows ON tv_shows.id = tv_genres_shows.tv_show_id
-LEFT JOIN tv_genres ON tv_genres_shows.tv_genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY title ASC, name ASC;
