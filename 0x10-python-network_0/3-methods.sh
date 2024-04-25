d73c61e774b80ce131e04956c7902546ad6b5558
Leornadia <leornadiajose@gmail.com>
2024-04-25 14:59:41 -0700

    3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# cURL display HTTP methods
-METHODS=$(curl -s -X OPTIONS -I "$1" | grep "Allow:" | cut -d " " -f2-)
-echo "$METHODS"
+# cURL display HTTP method 
+curl -sI "$1" | grep Allow | cut -d ' ' -f2-
