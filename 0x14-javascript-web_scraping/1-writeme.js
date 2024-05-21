9906984c74341331d946acfe1c6434a4c64c3a9a
Leornadia <leornadiajose@gmail.com>
2024-05-21 07:15:45 -0700

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-if (process.argv.length <= 3) {
+if (process.argv.length !== 4) {
   console.log('Usage: ./1-writeme.js file_path string_to_write');
   process.exit(1);
 }
@@ -13,6 +13,5 @@ const stringToWrite = process.argv[3];
 fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
   if (err) {
     console.log(err);
-    return;
   }
 });
